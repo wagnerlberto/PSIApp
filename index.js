18b8ffe171e5e3e06aef5b05d0ac3718554d52a8
prof. Wagner Berto <54516340+profwagnerberto@users.noreply.github.com>
2022-07-17 19:47:46 -0300

    adição de acessibilidade

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function toggleMenu() {
+function toggleMenu(event) {
+  if(event.type==='touchstart')
+    event.preventDefault();
   const nav = document.getElementById("nav");
   nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if(active)
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  else
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
 }
 
 const btnMobile = document.getElementById("btn-mobile");
